95d198ab62430a1525f69abdacc018d73bddc7df
Sebastian <39752847+sebastian0619@users.noreply.github.com>
2023-10-26 14:10:23 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
+  title: '翻译姬',
+  description: '核对翻译',
   copyright: '',
   privacy_policy: '',
   default_language: 'zh-Hans',
